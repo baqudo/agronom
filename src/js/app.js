267af2561d8fa59431c9f7d4e1abf84e354f30e6
BaQudo <baqudo@gmail.com>
2017-01-05 18:02:37 +0200

    1c
    1d

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// example of simple includes for js
+//=include lib/sayHello.js
+//=include lib/jquery.min.js
+//=include lib/slick.min.js
+//=include lib/svgxuse.min.js
+
+console.log('Hello, World!');
+
+$(document).ready(function (){
+	$('.slider').slick();
+});
+
+$(document).ready(function (){
+	$('.slider__bottom').slick({
+		slidesToShow: 4,
+	});
+});
