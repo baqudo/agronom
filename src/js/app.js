a22c8c4341117f58d080655251ba570487e625af
Aleksandr Balik <balikaleksandr@gmail.com>
2017-01-07 00:57:57 +0200

    ccc
    3d

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,34 @@ $(document).ready(function (){
 
 $(document).ready(function (){
 	$('.slider__bottom').slick({
-		slidesToShow: 3,
+		centerMode: true,
+		centerPadding: '350px',
+		autoplay: true,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		]
 	});
 });
 
